36cfbadc6278772b3a3d904bf0d94823e01b9f35
wagner <wagnerricardonet@gmail.com>
2019-09-09 13:11:17 -0300

    chore: listando categorias

--- src/main/java/com/wagnerww/pedidos/resources/CategoriaResources.java
+++ src/main/java/com/wagnerww/pedidos/resources/CategoriaResources.java
@@ -1,16 +1,28 @@
 package com.wagnerww.pedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wagnerww.pedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
