4db8828a1679c93e584f3c7e0c2683e40432e29e
wagner <wagnerricardonet@gmail.com>
2019-09-17 22:18:20 -0300

    chore: paginando os resultados e validações de entrada

--- src/main/java/com/wagnerww/pedidos/services/CategoriaService.java
+++ src/main/java/com/wagnerww/pedidos/services/CategoriaService.java
@@ -1,12 +1,19 @@
 package com.wagnerww.pedidos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wagnerww.pedidos.domain.Categoria;
+import com.wagnerww.pedidos.dto.CategoriaDTO;
 import com.wagnerww.pedidos.repositories.CategoriaRepository;
+import com.wagnerww.pedidos.services.exceptions.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -25,6 +32,39 @@ public class CategoriaService {
 					+", Tipo: "+Categoria.class.getName()));
 		//}
 	//	return obj;
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que  possui produtos!");
+		}
+		 
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
