f538bfd9410684cab244f5eb16135c2dd6b84f8d
wagner <wagnerricardonet@gmail.com>
2019-09-23 21:08:08 -0300

    chore: validators personalizados de edicao

---
+++ src/main/java/com/wagnerww/pedidos/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.wagnerww.pedidos.services.validation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.wagnerww.pedidos.domain.Cliente;
+import com.wagnerww.pedidos.domain.enums.TipoCliente;
+import com.wagnerww.pedidos.dto.ClienteDTO;
+import com.wagnerww.pedidos.dto.ClienteNewDTO;
+import com.wagnerww.pedidos.repositories.ClienteRepository;
+import com.wagnerww.pedidos.resources.exception.FieldMessage;
+import com.wagnerww.pedidos.services.validation.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
