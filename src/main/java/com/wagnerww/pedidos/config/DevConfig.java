9403fae77f05bc9cb434c1ce0e15ad9aa5d62e8e
wagner <wagnerricardonet@gmail.com>
2019-09-24 22:31:56 -0300

    chore: configurando pg e enviromente de ambiente

---
+++ src/main/java/com/wagnerww/pedidos/config/DevConfig.java
@@ -0,0 +1,37 @@
+package com.wagnerww.pedidos.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.wagnerww.pedidos.services.DBService;
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	//Pega  o valor da chave de dev
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		//Se estiver como create, alimenta os dados
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
